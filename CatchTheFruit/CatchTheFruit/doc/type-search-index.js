20892f5abc982a1c39936c4e743871a38f1a72d6
Piotr Nowakowski <piotrek.now11@gmail.com>
2020-05-28 21:00:35 +0200

    CTF

--- CatchTheFruit/CatchTheFruit/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Controller"},{"p":"sample","l":"Gra"},{"p":"sample","l":"Licznik"},{"p":"sample","l":"Main"},{"p":"sample","l":"Menu"},{"p":"sample","l":"Wyniki"}]
--- FIR/Debug/main.pp
+++
@@ -1,9 +0,0 @@
-# FIXED
-
-main.obj: ../main.c
-main.obj: ../include/dsplib.h
-main.obj: ../include/tms320.h
-
-../main.c: 
-../include/dsplib.h: 
-../include/tms320.h: 
